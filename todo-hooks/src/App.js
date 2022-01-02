5003e8d0db95c703209a8140cabcf82af3804be9
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-01-02 08:49:27 +0000

    Initial commit

---
+++ todo-hooks/src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import { useState } from "react";
+
+function App() {
+  const [currentItem, setCurrentItem ]= useState(null);
+
+  const onChangeHandler = (e) => {
+    console.log("current value", e.target.value);
+    setCurrentItem(e.target.value);
+
+  }
+  return (
+    <div className="App">
+    <header className="App-header">
+      <div className="Wrapper">
+        <div className="Input-wrapper">
+        <input value={currentItem} onChange={onChangeHandler} />
+        <button>+</button>
+        </div>
+      </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
